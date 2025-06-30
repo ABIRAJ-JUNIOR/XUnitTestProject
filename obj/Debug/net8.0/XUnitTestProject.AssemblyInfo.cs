fe9ed2771442b77e0ac0b4dff54c57c5f49ba2e7
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2025-06-30 12:27:19 +0530

    created a calculator class and add method

--- obj/Debug/net8.0/XUnitTestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/XUnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XUnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d9f582a933754741eb47fffa35c72196a27b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("XUnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XUnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
